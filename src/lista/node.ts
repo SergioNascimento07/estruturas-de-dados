9a6079a277e228bf2f3e4cf26db41c9902a39b36
SergioNascimento07 <sergioricjr72@gmail.com>
2023-07-28 13:36:50 -0300

    iniciando a desenvolver a estrutura de lista, estrutura de nós terminada, e desenvolvendo a lista em si

---
+++ src/lista/node.ts
@@ -0,0 +1,29 @@
+export class Node<T> {
+    private content: T;
+    private prevNode: Node<T> | null = null;
+    private nextNode: Node<T> | null = null;
+
+    public constructor(content: T) {
+        this.content = content
+    }
+
+    public setPrevNode(node: Node<T>|null): void {
+        this.prevNode = node
+    }
+
+    public setNextNode(node: Node<T>|null): void {
+        this.nextNode = node
+    }
+
+    public getPrevNode(): Node<T>|null {
+        return this.prevNode
+    }
+
+    public getNextNode(): Node<T>|null {
+        return this.nextNode
+    }
+}
+
+Node.prototype.toString = function nodeToString() {
+    return "d"
+}
